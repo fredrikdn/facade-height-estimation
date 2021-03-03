f612c969c16cdece0a52d4c9bdc991e61f061eb3
Fredrik <fredrikdn@gmail.com>
2021-03-03 14:24:54 +0100

    Floor seperation working adequately Some errors regarding distortion ==> RANSAC it 	modified:   ct.py 	modified:   heightutils.py 	modified:   sort.py

--- ct.py
+++ ct.py
@@ -10,65 +10,36 @@ from PIL import Image
 from myutils import *
 from heightutils import *
 
-# Iterate through the set of processed images
-
+# Processing of the images:
+imgs = "testimg/"
 folder = 'output/'
-pic = 'heidelberg-1.jpg'
+pic = 'trondheim-test.jpg'
+
 pic_c = pic + '.txt'
 pic_s = pic_c + '-sorted.txt'
 
+infile = folder + pic_c
+
 image = PIL.Image.open(folder + pic)
 
-file = 'output/' + pic_s
+sort_file = folder + pic_s
+
 start_time = time.time()
 runtime = 0
 
-# Calculations and assigning of objects into a sorted list
-
 # Lists
-object_list = []  # row: (0:x1, 1:y1, 2:x2, 3:y2, 4:class (Window: 0.0; Door: 1.0; Balcony: 2.0),
+objects = []  # row: (0:x1, 1:y1, 2:x2, 3:y2, 4:class (Window: 0.0; Door: 1.0; Balcony: 2.0),
                     # 5:threshold, 6:x_center, 7:y_center)
 
-length_list = []
-height_list = []  # list of heights (y value)
+lengths = []
+heights = []  # list of heights (y value)
+windows = []
 
 # Get width / height of the given pic
 width, height = image.size
 
-
-# Create a sorted list (array)
-with open(file, 'r') as sorted_file:
-    rows = sorted_file.readlines()
-    # for each processed object; do:
-    for row in rows:
-        splt = row.split()
-        tmp = []
-        i = 0
-        for s in splt:
-            tmp.append(float(s))
-            i = i + 1
-        object_list.append(tmp)
-
-        # Calculate and append length values for each window
-        if tmp[4] == 0.0:
-            x_diag = tmp[2] - tmp[0]
-            y_diag = tmp[3] - tmp[1]
-            length = math.sqrt(x_diag**2 + y_diag**2)
-            length_list.append(length)
-            height_list.append(y_diag)
-
-# TODO: Arrange image into network of objects and floors
-
-# OBJECT MANIPULATION: coordinates, center, etc.
-
-for obj in object_list:
-    x_center = obj[0] + ((obj[2] - obj[0]) / 2)
-    y_center = obj[1] + ((obj[3] - obj[1]) / 2)
-    obj.extend((x_center, y_center))  # add the center point coordinate to the object
-
-# WINDOW MANIPULATION
-
 # Average diagonal and height
+"""
 avg_diagonal = sum(length_list) / len(length_list)
 avg_height = sum(height_list) / len(height_list)
 y_max = max(height_list)
@@ -76,16 +47,15 @@ y_max = max(height_list)
 print('Window diagonal avg: ', avg_diagonal)
 print('Window height avg: ', avg_height)
 print('Window max height: ', y_max)
-
-window_list = []
-for o in object_list:
-    if o[4] == 0.0:
-        window_list.append(o)
+"""
 
 
 if __name__ == '__main__':
-    floors = detect_floors(window_list)
-    draw_floorlines(floors)
-    draw_centers(window_list, folder+pic)
+    #sort_y(infile)
+    objects, length, height, windows = sorted_array(sort_file)
+    floors = detect_floors(windows)
+    #draw_centers(windows, folder+pic)
+    #draw_floorlines(floors, folder+pic, width)
+    draw_lines_centers(windows, floors, imgs+pic, width)
     runtime = (time.time() - start_time)
     print("--- %s seconds ---" % runtime)
--- heightutils.py
+++ heightutils.py
@@ -1,5 +1,58 @@
 import numpy as np
 import cv2
+import math
+
+# ----------------------------- Sorting -------------------------------------
+
+
+def sort_y(file):  # Sort by Y
+
+    with open(file, 'r') as first_file:
+        rows = first_file.readlines()
+        sorted_rows = sorted(rows, key=lambda x: float(x.split()[1]), reverse=False)
+        with open(file + '-sorted.txt', 'w+') as second_file:
+            second_file.truncate(0)
+            for row in sorted_rows:
+                second_file.write(row)
+
+
+def sorted_array(file):  # Sort and create an array of coordinates
+    object_list = []
+    length_list = []
+    height_list = []
+    window_list = []
+    # Create a sorted list (array)
+    with open(file, 'r') as sorted_file:
+        rows = sorted_file.readlines()
+        # for each processed object; do:
+        for row in rows:
+            splt = row.split()
+            tmp = []
+            i = 0
+            for s in splt:
+                tmp.append(float(s))
+                i = i + 1
+            object_list.append(tmp)
+
+            # Calculate and append length values for each window
+            if tmp[4] == 0.0:
+                x_diag = tmp[2] - tmp[0]
+                y_diag = tmp[3] - tmp[1]
+                length = math.sqrt(x_diag ** 2 + y_diag ** 2)
+                length_list.append(length)
+                height_list.append(y_diag)
+
+    for obj in object_list:
+        x_center = obj[0] + ((obj[2] - obj[0]) / 2)
+        y_center = obj[1] + ((obj[3] - obj[1]) / 2)
+        obj.extend((x_center, y_center))  # add the center point coordinate to the object
+
+    for o in object_list:
+        if o[4] == 0.0:
+            window_list.append(o)
+
+    return object_list, length_list, height_list, window_list
+
 
 # ----------------------------- Floor utils -------------------------------------
 # FLOOR DETECTION
@@ -34,15 +87,16 @@ def detect_floors(objects):  # Detecting floors on the facade from object coordi
             # Constraint calculation
             slope = np.polyfit(x_list, y_list, 1)[0]
 
-            # Add the currently accumulated floor to its corresponding floor in f_list
+            # Add the currently accumulated floor of objects to its entry in the f_list
             if abs(slope) > constraint:
                 if not index == 0:
                     f_list.append(tmp_floor)
-                    tmp_floor = []
+                    tmp_floor.clear()
                     print("FLOOR LIST: ", f_list)
                 else:
                     f_list.append([ob])
 
+            # Continue adding objects to current floor
             else:
                 tmp_floor.append(ob)
                 print("Current Floor: ", tmp_floor)
@@ -90,21 +144,22 @@ def detect_floors(objects):  # Detecting floors on the facade from object coordi
 # Draw on building images for visualisation purposes
 
 
-def draw_centers(objects, img):
+def draw_centers(objects, img):  # Draw centers of objects
     image = cv2.imread(img, cv2.IMREAD_COLOR)
 
     for obj in objects:
-        image = cv2.circle(image, int((obj[6]), int(obj[7])), radius=0, color=(255, 0, 0), thickness=-1)
+        image = cv2.circle(image, (int(obj[6]), int(obj[7])), radius=2, color=(0, 0, 255), thickness=-1)
 
-    cv2.imwrite(image, "-centers.jpg")
+    name = img + "-centers.jpg"
+
+    if image is not None:
+        cv2.imwrite(name, image)
 
 
-def draw_floorlines(flrs):
-    facade = flrs
-
+def draw_floorlines(flrs, img, width):  # Draw floor lines
     line_list = []
 
-    for index, floor in enumerate(facade):
+    for index, floor in enumerate(flrs):
         xx = []
         yy = []
         for obj in floor:
@@ -116,5 +171,48 @@ def draw_floorlines(flrs):
         line = np.polyfit(x_list, y_list, 1)
         line_list.append(line)
 
+    image = cv2.imread(img, cv2.IMREAD_COLOR)
+    for line in line_list:
+        start_p = (0, int(line[1]))
+        end_p = (int(width), int(line[1]+(line[0]*width)))
+
+        cv2.line(image, start_p, end_p, (0, 255, 255), 1)
+
+    name = img + "-lines.jpg"
+
+    if image is not None:
+        cv2.imwrite(name, image)
+
+
+def draw_lines_centers(objects, flrs, img, width):
+    line_list = []
+
+    for index, floor in enumerate(flrs):
+        xx = []
+        yy = []
+        for obj in floor:
+            xx.append(obj[6])
+            yy.append(obj[7])
+        x_list = np.array(xx)
+        y_list = np.array(yy)
+
+        line = np.polyfit(x_list, y_list, 1)
+        line_list.append(line)
+
+    # DRAWING
+    image = cv2.imread(img, cv2.IMREAD_COLOR)
+    for line in line_list:
+        start_p = (0, int(line[1]))
+        end_p = (int(width), int(line[1] + (line[0] * width)))
+
+        cv2.line(image, start_p, end_p, (0, 255, 255), 1)
+
+    for obj in objects:
+        image = cv2.circle(image, (int(obj[6]), int(obj[7])), radius=2, color=(0, 0, 255), thickness=-1)
+
+    name = img + "-lines_points.jpg"
+
+    if image is not None:
+        cv2.imwrite(name, image)
 
 
